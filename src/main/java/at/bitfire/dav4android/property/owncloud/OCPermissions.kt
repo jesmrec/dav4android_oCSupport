a25483466d96232e17c488c25445ed508d11789b
Schabi <schabi@dibbler.solidgear.prv>
2018-05-10 14:32:19 +0200

    add popperties for owncloud

---
+++ src/main/java/at/bitfire/dav4android/property/owncloud/OCPermissions.kt
@@ -0,0 +1,29 @@
+package at.bitfire.dav4android.property.owncloud
+
+import android.content.pm.PackageManager
+import at.bitfire.dav4android.Property
+import at.bitfire.dav4android.PropertyFactory
+import at.bitfire.dav4android.XmlUtils
+import org.xmlpull.v1.XmlPullParser
+
+data class OCPermissions(
+        val permission:String
+): Property {
+
+    companion object {
+        @JvmField
+        val NAME = Property.Name(XmlUtils.NS_OWNCLOUD, "permissions")
+
+        class Factory: PropertyFactory {
+            override fun getName() = NAME
+
+            override fun create(parser: XmlPullParser) : OCPermissions? {
+                XmlUtils.readText(parser)?.let {
+                    return OCPermissions(it)
+                }
+                return null
+            }
+        }
+    }
+
+}
---
+++ src/main/java/at/bitfire/dav4android/property/owncloud/OCPrivatelink.kt
@@ -0,0 +1,27 @@
+package at.bitfire.dav4android.property.owncloud
+
+import at.bitfire.dav4android.Property
+import at.bitfire.dav4android.PropertyFactory
+import at.bitfire.dav4android.XmlUtils
+import org.xmlpull.v1.XmlPullParser
+
+
+class OCPrivatelink (
+        link : String
+): Property {
+    companion object {
+        @JvmField
+        val NAME = Property.Name(XmlUtils.NS_OWNCLOUD, "privatelink")
+
+        class Factory: PropertyFactory {
+            override fun getName() = OCPermissions.NAME
+
+            override fun create(parser: XmlPullParser) : OCPrivatelink? {
+                XmlUtils.readText(parser)?.let {
+                    return OCPrivatelink(it)
+                }
+                return null
+            }
+        }
+    }
+}
