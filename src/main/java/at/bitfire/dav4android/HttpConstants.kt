a149a3ff92a1149f02012086ed4de4b442efece5
davigonz <dgonzalez@solidgear.es>
2018-05-24 13:45:13 +0200

    Reinclude DavOCResource

--- src/main/java/at/bitfire/dav4android/HttpConstants.kt
+++ src/main/java/at/bitfire/dav4android/HttpConstants.kt
@@ -2,3 +2,6 @@ package at.bitfire.dav4android
 
 const val IF_MATCH_HEADER = "If-Match"
 const val IF_NONE_MATCH_HEADER = "If-None-Match"
+const val CONTENT_TYPE_HEADER = "Content-Type"
+const val OC_TOTAL_LENGTH_HEADER = "OC-Total-Length"
+const val OC_X_OC_MTIME_HEADER = "X-OC-Mtime"
